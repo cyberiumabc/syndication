4c2b0c24317572f4f0d8ac6945c3bddcd7c7117d
Prabhat kumar SINGH <prabhatkr@gmail.com>
2020-04-21 14:35:28 -0400

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+var GenesisInventory = [
+  {"name":"SOLYD", "id":"SYD", "icon":"assets/solyd.png"},
+  {"name":"BITCOIN", "id":"ETH", "icon":"assets/bitcoin.png"},
+  {"name":"ETHEREUM", "id":"ETH", "icon":"assets/ethereum.png"}
+]
+
+
+
+exports GenesisInventory;
